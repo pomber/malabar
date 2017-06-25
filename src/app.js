d1e4faf32697f275aae30acf1fc27295550f1461
pomber <pombopombopombo@gmail.com>
2017-06-25 19:12:09 -0300

    Format code

--- src/app.js
+++ src/app.js
@@ -1,36 +1,46 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
-  70,
-  window.innerWidth / window.innerHeight,
-  1,
-  1000
-);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth*3/4, window.innerHeight*3/4);
+const { Scene, PerspectiveCamera, WebGLRenderer, Vector3 } = THREE;
+const { Mesh, BoxGeometry, MeshBasicMaterial } = THREE;
+
+const W = window.innerWidth;
+const H = window.innerHeight;
+const GY = -9.8;
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(70, W / H, 1, 1000);
+const renderer = new WebGLRenderer();
+renderer.setSize(W, H);
+
 document.body.appendChild(renderer.domElement);
 
-// Physics variables
-var gravityConstant = -9.8;
+let cube, fixo;
+init();
+animate();
 
+function init() {
+  cube = new Mesh(
+    new BoxGeometry(1, 1, 1),
+    new MeshBasicMaterial({
+      color: 0x0ffff0,
+      wireframe: true
+    })
+  );
+  scene.add(cube);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x0ffff0, wireframe: true } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+  fixo = new Mesh(
+    new BoxGeometry(0.5, 0.5, 0.5),
+    new MeshBasicMaterial({ color: 0xffff00 })
+  );
+  scene.add(fixo);
 
-geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
-material = new THREE.MeshBasicMaterial( { color: 0xffff00, } );
-var fixo = new THREE.Mesh( geometry, material );
-scene.add( fixo);
-
-camera.position.z = 3;
-camera.lookAt(new THREE.Vector3(0,1,0));
+  camera.position.y = -1;
+  camera.position.z = 3;
+  camera.lookAt(new Vector3(0, 1, 0));
+}
 
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
   //cube.rotation.x += 0.01;
   cube.rotation.y += 0.005;
   fixo.position.y += 0.005;
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
-animate();
